483d2bbb89e7d6b48a2e563dd46bc374fa5ab07d
collinsevan <collinsevan33@gmail.com>
2026-01-05 15:11:30 +0000

    Customise products admin

--- products/admin.py
+++ products/admin.py
@@ -7,7 +7,12 @@ class CategoryAdmin(admin.ModelAdmin):
     """
     Admin configuration for product categories.
     """
-    list_display = ()
+    list_display = (
+        "friendly_name",
+        "name",
+        "slug",
+    )
+    ordering = ("name",)
 
 
 @admin.register(Product)
@@ -15,4 +20,13 @@ class ProductAdmin(admin.ModelAdmin):
     """
     Admin configuration for products.
     """
-    list_display = ()
+    list_display = (
+        "sku",
+        "name",
+        "category",
+        "price",
+        "rating",
+        "license_type",
+        "is_digital",
+    )
+    ordering = ("sku",)
