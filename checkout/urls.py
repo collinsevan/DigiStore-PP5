829c20287d4d48533cc358f3831f3636635aabd5
collinsevan <collinsevan33@gmail.com>
2026-03-16 15:37:18 +0000

    Add Stripe webhook listener setup
    Created checkout/webhook_handler.py with generic and payment intent webhook handler methods.
    
    Added checkout/webhooks.py to verify Stripe webhook events and route them to the handler.
    
    Updated checkout/urls.py to include the Stripe webhook endpoint at checkout/wh/.

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
+
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
     path("", views.checkout, name="checkout"),
-    path("success/<reference>/", views.checkout_success, name="checkout_success"),
+    path(
+        "success/<reference>/",
+        views.checkout_success,
+        name="checkout_success",
+    ),
+    path("wh/", webhook, name="webhook"),
 ]
