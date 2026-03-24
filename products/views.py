2bffe297b7e40928fbad3bc753adfefb3cd545e9
collinsevan <collinsevan33@gmail.com>
2026-03-24 14:38:47 +0000

    Add product management form and create product views and urls for product management

--- products/views.py
+++ products/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.db.models.functions import Lower
 
+from .forms import ProductForm
 from .models import Product, Category
 
 
@@ -81,3 +83,40 @@ def product_detail(request, product_id):
     }
 
     return render(request, "products/product-detail.html", context)
+
+
+@login_required
+def add_product(request):
+    """
+    Add a product to the store.
+    """
+    if not request.user.is_superuser:
+        messages.error(
+            request,
+            "Sorry, only store owners can do that."
+        )
+        return redirect(reverse("home"))
+
+    if request.method == "POST":
+        form = ProductForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            product = form.save()
+            messages.success(request, "Product added successfully.")
+            return redirect(
+                reverse("product_detail", args=[product.id])
+            )
+
+        messages.error(
+            request,
+            "Failed to add product. Please ensure the form is valid."
+        )
+
+    else:
+        form = ProductForm()
+
+    context = {
+        "form": form,
+    }
+
+    return render(request, "products/add_product.html", context)
