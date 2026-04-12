8b496e9a7a9243940a307014fd4f372f59efd32d
collinsevan <collinsevan33@gmail.com>
2026-04-12 21:01:21 +0100

    Fix AWS static file cache settings

--- digistore/settings.py
+++ digistore/settings.py
@@ -191,8 +191,7 @@ if 'USE_AWS' in os.environ:
 
     # S3 file caching
     AWS_S3_OBJECT_PARAMETERS = {
-        "Expires": "Thu, 31 Dec 2099 20:00:00 GMT",
-        "CacheControl": "max-age=94608000",
+        "CacheControl": "max-age=0, no-cache, no-store, must-revalidate",
     }
 
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
